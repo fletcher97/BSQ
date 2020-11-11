6b887ef942706233d4d1263531744197b352b4f3
Filipe Heaton ponce cabral pinto <fheaton-@c1r18s1.42lisboa.com>
2020-11-11 23:42:35 +0000

    format

--- src2/write_map.c
+++ src2/write_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   write_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fheaton- <fheaton-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 23:33:03 by fheaton-          #+#    #+#             */
-/*   Updated: 2020/11/11 23:22:20 by mgueifao         ###   ########.fr       */
+/*   Updated: 2020/11/11 23:41:50 by fheaton-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ extern int g_error;
 
 int		put_full(int a, int b, t_map *map)
 {
-	if (a >= (map->pos % map->width) && a <= ((map->pos + map->biggest_size - 1) % map->width)
-		&& b >= (map->pos / map->width) && b <= ((map->pos + (map->biggest_size - 1) * map->width) / map->width))
+	if (a >= (map->pos % map->width) && a <=\
+		((map->pos + map->biggest_size - 1) % map->width)\
+		&& b >= (map->pos / map->width) && b <= ((map->pos\
+		+ (map->biggest_size - 1) * map->width) / map->width))
 		return (1);
 	else
 		return (0);
@@ -25,8 +27,10 @@ int		put_full(int a, int b, t_map *map)
 
 int		put_obstacle(int a, int b, t_map *map)
 {
-	if ((a == 0 && b == 0 && (map->obs_count[a] == 1)) || (b == 0 && a != 0 && (map->obs_count[a] > map->obs_count[a - 1])) || \
-				(a == 0 && b != 0 && (map->obs_count[b * map->width] > map->obs_count[(b - 1) * map->width])))
+	if ((a == 0 && b == 0 && (map->obs_count[a] == 1)) ||\
+	(b == 0 && (map->obs_count[a] > map->obs_count[a - 1])) || \
+				(a == 0 && (map->obs_count[b * map->width]\
+				> map->obs_count[(b - 1) * map->width])))
 		return (1);
 	else
 		return (0);
@@ -34,11 +38,24 @@ int		put_obstacle(int a, int b, t_map *map)
 
 int		put_empty(int a, int b, t_map *map)
 {
-	if ((a == 0 && b == 0) || (b == 0 && (map->obs_count[a] == map->obs_count[a - 1])) || \
-		(a == 0 && (map->obs_count[b * map->width] == map->obs_count[(b - 1) * map->width])))
+	if ((a == 0 && b == 0) || (b == 0 &&\
+		(map->obs_count[a] == map->obs_count[a - 1])) || \
+		(a == 0 && (map->obs_count[b * map->width] ==\
+		map->obs_count[(b - 1) * map->width])))
 		return (1);
 	else
 		return (0);
+}
+
+void	put_rest(int a, int b, t_map *map)
+{
+	if (map->obs_count[a + b * map->width]\
+		- map->obs_count[(a - 1) + b * map->width]
+		- map->obs_count[a + (b - 1) * map->width] + map->obs_count[a\
+		- 1 + (b - 1) * map->width])
+		ft_putchar(map->obstacle);
+	else
+		ft_putchar(map->empty);
 }
 
 int		write_map(t_map *map)
@@ -59,11 +76,7 @@ int		write_map(t_map *map)
 			else if (put_empty(a, b, map))
 				ft_putchar(map->empty);
 			else
-				if (map->obs_count[a + b * map->width] - map->obs_count[(a - 1) + b * map->width]
-					- map->obs_count[a + (b - 1) * map->width] + map->obs_count[a - 1 + (b - 1) * map->width])
-					ft_putchar(map->obstacle);
-				else
-					ft_putchar(map->empty);
+				put_rest(a, b, map);
 		}
 		ft_putchar('\n');
 	}
