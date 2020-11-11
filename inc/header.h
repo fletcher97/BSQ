14bede98355016a0cba5cbb119b6aae2f184c2cb
Miguel Gueifao <mgueifao@student.42lisboa.com>
2020-11-11 23:35:13 +0000

    Norminette fix

--- inc/header.h
+++ inc/header.h
@@ -3,19 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   header.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fheaton- <fheaton-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 22:04:43 by fheaton-          #+#    #+#             */
-/*   Updated: 2020/11/11 22:04:43 by fheaton-         ###   ########.fr       */
+/*   Updated: 2020/11/11 23:34:53 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MAP_H
+#ifndef HEADER_H
 # define HEADER_H
 
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
+
 /*
 ** obstacul list
 ** empty char
@@ -30,18 +31,18 @@
 
 typedef struct	s_map
 {
-	int		*obs_count;
-	char	empty;
-	char	obstacle;
-	char	full;
-	int		height;
-	int		width;
-	int		biggest_size;
-	int		pos;
-	int		valid;
+	int			*obs_count;
+	char		empty;
+	char		obstacle;
+	char		full;
+	int			height;
+	int			width;
+	int			biggest_size;
+	int			pos;
+	int			valid;
 }				t_map;
 
-extern int	g_error;
+extern int		g_error;
 
 /*
 ** error code map.valid (Currently not in use)
@@ -60,41 +61,41 @@ extern int	g_error;
 ** map.c
 */
 
-t_map		*init(int fd);
-int			read_solve_map(int fd);
+t_map			*init(int fd);
+int				read_solve_map(int fd);
 
 /*
 ** process.c
 */
 
-int			process(t_map *map);
+int				process(t_map *map);
 
 /*
 ** str.c
 */
 
-int			str_len(char *str);
-void		ft_putstr(char *c);
-void		ft_putchar(char c);
+int				str_len(char *str);
+void			ft_putstr(char *c);
+void			ft_putchar(char c);
 
 /*
 ** reader.c
 */
 
-t_map		*read_map(int fd);
-char		*read_line(int fd, int pos);
-int			clear_map(t_map *map);
+t_map			*read_map(int fd);
+char			*read_line(int fd, int pos);
+int				clear_map(t_map *map);
 
 /*
 ** solver.c
 */
 
-void		check_g_error(void);
+void			check_g_error(void);
 
 /*
 ** writefile.c
 */
 
-int			write_map(t_map *map);
+int				write_map(t_map *map);
 
 #endif
