c8a4cc5dab4038c1cb6d5434324e01b96aee6f06
Filipe Heaton ponce cabral pinto <fheaton-@c1r18s1.42lisboa.com>
2020-11-11 19:07:18 +0000

    aaaaaA

--- inc/reader.h
+++
@@ -1,9 +0,0 @@
-#ifndef READER_H
-# define READER_H
-
-#include "map.h"
-
-t_map	*read_map(int fd);
-char	*read_line(int fd, int pos);
-
-#endif
--- inc/str.h
+++
@@ -1,8 +0,0 @@
-#ifndef STR_H
-# define STR_H
-
-int	str_len(char *str);
-void ft_putstr(char *c);
-void ft_putchar(char c);
-
-#endif
